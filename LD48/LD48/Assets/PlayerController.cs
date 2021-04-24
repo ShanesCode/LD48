3007d8aed8c99ab8caacbcd0b3b871632163af8d
Shane Massaad <shanemassaad@hotmail.com>
2021-04-24 13:11:23 +0100

    Change to using Quaternion.LookRotation instead of my maths functions which led to gimbal locking issues

--- LD48/LD48/Assets/PlayerController.cs
+++ LD48/LD48/Assets/PlayerController.cs
@@ -4,23 +4,89 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float sinkForce;
-    public float moveForce;
+    public float sinkSpeed;
+    public float moveSpeed;
+    
     public int playerHealth;
 
     public GameObject worldCenter;
 
-    private Rigidbody2D rb;
+    private float moveX;
+    private float moveY;
+    private Vector2 moveVec;
+    
+    public float maxSpeed;
+
+    private Rigidbody2D rb2d;
+
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        rb.AddForce((gameObject.transform.position - worldCenter.transform.position) * -sinkForce, ForceMode2D.Force);
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            moveY = Input.GetAxis("Vertical") * moveSpeed;
+        }
+        else
+        {
+            if (moveY > -sinkSpeed)
+            {
+                // Sink
+                moveY -= sinkSpeed / 100;
+            } else
+            {
+                moveY += sinkSpeed / 100;
+            }
+        }
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            moveX = Input.GetAxis("Horizontal") * moveSpeed;
+        }
+        else
+        {
+            if (moveX != 0)
+            {
+                if (moveX < 0)
+                {
+                    moveX += maxSpeed / 100;
+                }
+                else
+                {
+                    moveX -= maxSpeed / 100;
+                }
+            }
+        }
+
+        moveVec = moveX * transform.right + moveY * transform.up;
+        rb2d.AddForce(moveVec, ForceMode2D.Force);
+        RotateToWorldCenter();
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Vector3 v1 = moveX * transform.right;
+        Gizmos.DrawLine(transform.position, transform.position + v1);
+
+        Gizmos.color = Color.blue;
+        Vector3 v2 = moveY * transform.up;
+        Gizmos.DrawLine(transform.position, transform.position + v2);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, worldCenter.transform.position);
+    }
+
+    void RotateToWorldCenter()
+    {
+        Vector2 vecToWorldCenter = transform.position - worldCenter.transform.position;
+
+        Quaternion rotation = Quaternion.LookRotation(Vector3.forward, vecToWorldCenter);
+        transform.rotation = rotation;
     }
 }
