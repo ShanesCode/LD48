cd3fe6d71ef3173ce880ca189468a16cc1716ae5
villaindotlove <81622260+villaindotlove@users.noreply.github.com>
2021-04-24 16:51:41 +0100

    Script for floater enemy

---
+++ LD48/LD48/Assets/Floater.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floater : MonoBehaviour
+{
+    float currentY;
+
+    float floatRange = 1.0f;
+    float floatSpeed = 0.5f;
+    bool travellingUp = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentY = 0;
+        Vector3 brainTransform = GameObject.Find("Brain").transform.position;
+        transform.rotation = RotateToWorldCenter(brainTransform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Float();
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.name == "Player"){
+            Debug.Log(gameObject.name + " had a collision with the player!");
+            //add damage
+            //add sfx
+        }
+    }
+
+    void Float()
+    {
+        float increment = Time.deltaTime * floatSpeed;
+        if (travellingUp)
+        {
+            gameObject.transform.Translate(Vector3.up * increment);
+            currentY += increment;
+        }
+        else
+        {
+            gameObject.transform.Translate(Vector3.down * increment);
+            currentY -= increment;
+        }
+
+        if ((currentY > floatRange) || (currentY < -floatRange))
+        {
+            travellingUp = !travellingUp;
+        }
+    }
+
+    Quaternion RotateToWorldCenter(Vector3 worldCenter)
+    {
+        Vector2 vecToWorldCenter = transform.position - worldCenter;
+        return Quaternion.LookRotation(Vector3.forward, vecToWorldCenter);
+    }
+}
